6ec86bdbb23956dff335c7b9d8a4b4732d3faf62
Marcelo Valois <mmv147.valois@gmail.com>
2022-10-04 23:51:38 -0300

    Task 2 feita

--- Task2.java
+++ Task2.java
@@ -1,16 +1,21 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class Task2
 {
 	public static void main(String[] args) throws IOException {
         Stack <String> rpn = new Stack <String> ();
+        List<String> tokens = new ArrayList<String>();
 		FileReader fr = new FileReader("Calc1.stk");
 		BufferedReader br = new BufferedReader(fr);
 		
         String line;
+        Token token;
+        boolean error = false;
 		try {
             do {
                 line = br.readLine();
@@ -19,24 +24,46 @@ public class Task2
                     int a = Integer.parseInt(rpn.pop());
 					int b = Integer.parseInt(rpn.pop());
                     rpn.push(String.valueOf(a*b));
+                    token = new Token(TokenType.STAR, "*");
+                    tokens.add(token.toString());
                 }
                 else if(line.equals("+")) {
 					int a = Integer.parseInt(rpn.pop());
 					int b = Integer.parseInt(rpn.pop());
 					rpn.push(String.valueOf(a+b));
+
+                    token = new Token(TokenType.PLUS, "+");
+                    tokens.add(token.toString());
 				}
 				else if(line.equals("-")) {
 					int a = Integer.parseInt(rpn.pop());
 					int b = Integer.parseInt(rpn.pop());
 					rpn.push(String.valueOf(a-b));
+
+                    token = new Token(TokenType.MINUS, "-");
+                    tokens.add(token.toString());
 				}
 				else if(line.equals("/")) {
 					int a = Integer.parseInt(rpn.pop());
 					int b = Integer.parseInt(rpn.pop());
 					rpn.push(String.valueOf(a/b));
+
+                    token = new Token(TokenType.SLASH, "/");
+                    tokens.add(token.toString());
 				}
 				else {
-					rpn.push(line);
+                    try {
+                        int aux = Integer.parseInt(line);
+
+                        rpn.push(line);
+    
+                        token = new Token(TokenType.NUM, line);
+                        tokens.add(token.toString());
+
+                    } catch (Exception e) {
+                        error = true;
+                        System.out.println("Error: Unexpected character: " + line);
+                    }
 				}
 
             } while (line != null);
@@ -47,8 +74,42 @@ public class Task2
 		} finally {
             br.close();
             fr.close();
+
+            if (!error) {
+                for (String i : tokens) {
+                    System.out.println(i);
+                }
+            }
             System.out.println(rpn.pop());
 		}
 		
 	}
+}
+
+class Token {
+
+	public final TokenType type; // token type
+	public final String lexeme; // token value
+
+	public Token (TokenType type, String value) {
+		this.type = type;
+		this.lexeme = value;
+	}
+
+	@Override
+	public String toString() {
+		return "Token [type=" + this.type + ", lexeme=" + this.lexeme + "]";
+	}
+}
+
+enum TokenType {
+
+	// Literals.
+	NUM,
+
+	// Single-character tokens for operations.
+	MINUS, PLUS, SLASH, STAR,
+	
+	EOF
+
 }
